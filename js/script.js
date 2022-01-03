af035a2315dfd5e0774b83b910ab356d99ac0da8
EsauKip <esau.kiprono@student.moringaschool.com>
2022-01-03 10:37:41 +0300

    validate form fields

--- js/script.js
+++ js/script.js
@@ -22,3 +22,41 @@ $(document).ready(function() {
         })
 
 });
+var contactUsValidation = function() {
+
+    var name = document.getElementById("userName").value;
+    var email = document.getElementById("userEmail").value;
+    var sms = document.getElementById("message").value;
+
+    // validation
+
+    if (name.length < 2 || name == " ") {
+        alert("Please enter a valid name! \nName must be filled")
+        return false;
+    };
+
+    atPosition = email.indexOf("@");
+    dotPosition = email.lastIndexOf(".");
+
+    if (email == " ") {
+        alert("Email must be filled!")
+        return false;
+    };
+
+    if (atPosition <= 2) {
+        alert("Provide a valid Email address!")
+        return false;
+    };
+
+    if (dotPosition == -1) {
+        alert("Provide a valid email address.")
+        return false;
+    };
+
+    if (sms == " " || sms < 2) {
+        alert("You have not entered any message, or your message is too short.")
+        return false;
+    } else {
+        alert("Hi " + name + ", your message was successfully sent. Thank you for reaching out to us.")
+    }
+};
